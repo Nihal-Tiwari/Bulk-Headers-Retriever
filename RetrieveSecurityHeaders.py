482f79e56624bcbb78c2fdf135560a05c7b64103
Nihal Tiwari <77505233+Nihal-Tiwari@users.noreply.github.com>
2023-10-16 18:52:24 +0530

    Added SSL verification as an option

--- RetrieveSecurityHeaders.py
+++ RetrieveSecurityHeaders.py
@@ -5,18 +5,34 @@ import json
 from urllib.parse import urlparse
 import sys
 
+# Disable all requests and urllib3 warnings
+requests.packages.urllib3.disable_warnings()
+
 # Function to retrieve and return security response headers
-def get_security_response_headers(url):
+def get_security_response_headers(url, disable_ssl_verify=False):
     try:
+        # Check if the URL lacks a scheme (http or https) and add "https://"
+        parsed_url = urlparse(url)
+        if not parsed_url.scheme:
+            url = "https://" + url
+
         # Set the user agent header
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
         }
-        
-        response = requests.head(url, allow_redirects=True, headers=headers)
+
+        # Determine whether to verify SSL certificates
+        verify_ssl = not disable_ssl_verify
+
+        response = requests.head(url, allow_redirects=True, headers=headers, verify=verify_ssl)
         final_url = response.url 
-        if response.status_code == 200:
-            # Specify the list of security headers to capture
+        status_code = response.status_code  
+
+        # Print the status code
+        print(f"URL: {final_url}, Status Code: {status_code}")
+
+        if status_code == 200:
+            # Specify the list of security headers to retrieve
             security_headers = [
                 "strict-transport-security",
                 "x-frame-options",
@@ -31,15 +47,15 @@ def get_security_response_headers(url):
                 "cache-control",
                 "permissions-policy"
             ]
-            
-            # Capture security response headers
-            headers_dict = {header: response.headers.get(header, "Not set") for header in security_headers}
 
-            return final_url, headers_dict
+            # Capture security response headers
+            headers_dict = {header: response.headers.get(header, "HEADER_NOT_SET") for header in security_headers}
+
+            return final_url, headers_dict, status_code
         else:
-            return final_url, {"Status Code": response.status_code, "Error": "Failed to fetch headers"}
+            return url, {"Status Code": status_code, "Error": "Failed to fetch headers"}, status_code
     except requests.exceptions.RequestException as e:
-        return url, {"Error": str(e)}
+        return url, {"Error": str(e)}, None
 
 def save_to_csv(data, output_file):
     df = pd.DataFrame(data)
@@ -54,12 +70,14 @@ def main():
     parser.add_argument("input_file", help="Path to the input text file containing one website URL per line")
     parser.add_argument("output_file", help="Path to the output file where headers will be saved")
     parser.add_argument("--format", choices=["csv", "json"], default="csv", help="Output format (csv or json, default is csv)")
+    parser.add_argument("--disable-ssl-verify", action="store_true", help="Disable SSL certificate verification")
 
     args = parser.parse_args()
 
     input_file = args.input_file
     output_file = args.output_file
     output_format = args.format
+    disable_ssl_verify = args.disable_ssl_verify
 
     try:
         with open(input_file, 'r') as file:
@@ -68,8 +86,9 @@ def main():
         all_headers = []
 
         for website in websites:
-            url, headers = get_security_response_headers(website)
-            all_headers.append({"URL": url, **headers})
+            url, headers, status_code = get_security_response_headers(website, disable_ssl_verify)
+            entry = {"URL": url, **headers, "Status Code": status_code}
+            all_headers.append(entry)
 
             # Real-time printing of progress
             sys.stdout.write("\rRetrieving security headers... ")
